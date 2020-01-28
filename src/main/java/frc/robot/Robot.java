33c937aac6a37e4384015efa75a1fa84fdb0bb09
Teraterayuki <tenko0316yuki1@gmail.com>
2020-01-28 19:33:01 +0900

    renew

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,91 +7,83 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
-public class Robot extends IterativeRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+//TalonSRX&VictorSPXのライブラリー
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
+public class Robot extends TimedRobot {
+
+  //コントローラー
+  //とりあえず、Xbox2つ
+  XboxController driver, operator;
+
+  //DriveMotor
+  WPI_TalonSRX  driveRightFront,driveLeftFront;
+  VictorSPX driveRightBack,driveLeftBack;
+
+  //センサー
+  ADXRS450_Gyro gyro;
+
+  //SubClass
+  Drive drive;
+  State state;
+
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
+
+    //コントローラーの初期化
+    driver = new XboxController(Const.DriveControllerPort);
+    operator = new XboxController(Const.OperateControllerPort);
+
+    //gyroの初期化
+    gyro = new ADXRS450_Gyro();
+
+    //DriveMotor
+    //ドライブモーターの初期化
+    driveRightFront = new WPI_TalonSRX(Const.DriveRightFrontPort);
+    driveRightBack = new VictorSPX(Const.DriveRightBackPort);
+    driveLeftFront = new WPI_TalonSRX(Const.DriveLeftFrontPort);
+    driveLeftBack = new VictorSPX(Const.DriveLeftBackPort);
+    //ドライブモーターの台形加速&フォローの設定
+    driveLeftFront.configOpenloopRamp(Const.DriveFullSpeedTime);
+    driveLeftBack.follow(driveLeftFront);
+    driveRightFront.configOpenloopRamp(Const.DriveFullSpeedTime);
+    driveRightBack.follow(driveRightFront);
+
+
+    drive = new Drive(driveLeftFront, driveRightFront, gyro);
+    state = new State();
+
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
   @Override
   public void robotPeriodic() {
   }
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
-   */
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // autoSelected = SmartDashboard.getString("Auto Selector",
-    // defaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
   }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
   }
 
-  /**
-   * This function is called periodically during operator control.
-   */
   @Override
   public void teleopPeriodic() {
-  }
 
-  /**
-   * This function is called periodically during test mode.
-   */
+        /********** Drive ***********/
+          state.driveState = State.DriveState.kManual;
+          state.driveStraightSpeed = Util.deadbandProcessing(driver.getY(Hand.kLeft));
+          state.driveRotateSpeed = Util.deadbandProcessing(driver.getX(Hand.kRight));
+
+          drive.apllyState(state);
+        }
+
   @Override
   public void testPeriodic() {
   }
---
+++ src/main/java/frc/robot/Util.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public class Util {
+
+    //不感帯処理
+    public static double deadbandProcessing(double value) {
+        return Math.abs(value) > Const.Deadband ? value : 0 ;
+    }
+
+}
