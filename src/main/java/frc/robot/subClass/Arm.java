6c4f0d76fdf461795b29928a6e0ed9f9d69de168
Hirose-Yuto <yuto.h@sakura-temepsta.org>
2020-02-28 15:40:21 +0900

    Canon→Arm Robot.javaのArmの引数書いて State.javaのInitもチェックしといて Const.javaの値も変更しといて

--- src/main/java/frc/robot/subClass/Arm.java
+++ src/main/java/frc/robot/subClass/Arm.java
@@ -1,78 +1,210 @@
+
 package frc.robot.subClass;
 
+//import edu.wpi.first.wpilibj.Encoder;
+//import edu.wpi.first.wpilibj.DigitalInput;
+//import edu.wpi.first.wpilibj.SpeedController;
+//import edu.wpi.first.wpilibj.PIDController;
+//import edu.wpi.first.wpilibj.Talon;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.SensorCollection;
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import edu.wpi.first.wpilibj.Encoder;
+import com.ctre.phoenix.motorcontrol.DemandType;
 
-public class Arm {
+public class Arm{
 
     //宣言
-    Encoder Encoder;            //エンコーダー（多分使わない）
-    TalonSRX Motor;      //モーター
-    ArmSensor armSensor;
+    SensorCollection Encoder;   //エンコーダー
+    WPI_TalonSRX Motor;      //モーター
+
+    SensorCollection MaxUp;         //角度上限(Fwd)
+    SensorCollection MaxDown;       //角度下限（0度であり基本状態）(Rev)
+
 
     //コンストラクター
-    public Arm(TalonSRX ArmMotor, ArmSensor armSensor) {
+    public Arm(WPI_TalonSRX ArmMotor, SensorCollection ArmEncoder,
+               SensorCollection MaxUpSwitch, SensorCollection MaxDownSwitch){
+
         this.Motor = ArmMotor;
-        this.armSensor = armSensor;
+        this.Encoder = ArmEncoder;
+
+        this.MaxUp = MaxUpSwitch;
+        this.MaxDown = MaxUpSwitch;
+
     }
 
-    //現在の砲台の状態を確認
-    public void applyState(State state) {
-        switch (state.armState) {
-            case k_Basic:
-                setArmSpeed(state.armMotorSpeed);
+    //----------------------------------------------------------------------------------------------------
+    //現在の砲台の状態を確認（行き過ぎを防ぐ）(DownがRev、UpがVel)（最も上を向いているときが0度、最も下が135度）
+    void ArmCheck(State state){
+
+        if(MaxUp.isFwdLimitSwitchClosed()){
+            //角度上限スイッチがONの時、砲台は基本状態である
+            state.armState = State.ArmState.k_Maxup;
+
+            //現在角度は0度
+            state.armAngle = 0;
+        }
+        else if(MaxDown.isRevLimitSwitchClosed()){
+            //角度下限スイッチがONの時、砲台は最も上を向いている
+            state.armState = State.ArmState.k_Basic;
+
+            //現在角度は135度
+            state.armAngle = Const.ArmMaxAngle;
+        }
+        else{
+            //角度上限スイッチも下限スイッチも押されていない状態
+            state.armState = State.ArmState.k_Aiming;
+
+            //現在角度にエンコーダーからの値を計算して代入
+            state.armAngle = getArmNow(Encoder.getAnalogInRaw());
+        }
+    }
+
+
+    //---------------------------------------------------------------------------------------------------
+    //出力処理
+    void applyState(State state){
+        //まず、最初に砲台の状態を確認
+        ArmCheck(state);
+
+        switch(state.armOutState){
+            //---------------------------------------------------------------
+            //砲台の角度をゼロに
+            case k_ChangeBasic:
+
+
+
                 break;
-            case k_Maxup:
+            //---------------------------------------------------------------
+            //砲台の角度をセル発射用に
+            case k_Shoot:
+
+
+
                 break;
-            case k_Aiming:
+            //---------------------------------------------------------------
+            //砲台の角度をパネル回転用に
+            case k_Panel:
+
+
+
                 break;
+            //---------------------------------------------------------------
+            //砲台の角度を微調整 
+            case k_LittleAaim:
+                ArmAiming(state);
+                break;
+            //---------------------------------------------------------------
+            //砲台の角度を地面と平行に（使う？） 
+            case k_Parallel:
+
+                break;
+            //---------------------------------------------------------------
+            //何もしない
+            case k_DoNothing:
+
+                break;
+
         }
     }
 
     //--------------------------------------------------------------------------------
-    //砲台のモーターを回す(速さはsetSpeedで決める)
-    public void setArmSpeed(double setSpeed) {
-        Motor.set(ControlMode.PercentOutput, setSpeed);
+    //砲台の角度を微調整する（PID無し）
+    void ArmAiming(State state){
+
+        //砲台の状態に合わせて微妙に動く
+        switch(state.armState){
+            //------------------------------------------
+            //角度が初期状態（0度）
+            case k_Basic:
+                if(state.armRotateSpeed > 0){
+                    ArmMove(state.armRotateSpeed * Const.ArmMagni);
+                    break;
+                }
+                break;
+            //------------------------------------------
+            //角度が最も上（135度）
+            case k_Maxup:
+                if(state.armRotateSpeed < 0){
+                    ArmMove(state.armRotateSpeed * Const.ArmMagni);
+                    break;
+                }
+                break;
+            //------------------------------------------
+            //角度が0度～135度
+            case k_Aiming:
+                ArmMove(state.armRotateSpeed * Const.ArmMagni);
+                break;
+            //------------------------------------------
+        }
     }
 
+    //--------------------------------------------------------------------------------
+    //砲台のモーターを回す制御(速度をsetSpeedで決める)（）
+    void ArmMove(double setSpeed){
+        Motor.set(setSpeed);
+    }
+
+
+    //砲台のモーターを回すPID制御(位置をSetPoint()で決める・重力オフセットをSetFeedForward()で決める)
+    void ArmPIDMove(int TargetAngle){
+
+        Motor.set(ControlMode.Position, SetPoint(TargetAngle),
+                DemandType.ArbitraryFeedForward, SetFeedForward(TargetAngle));
+
+    }
 
     //--------------------------------------------------------------------------------
     //砲台の位置を管理
 
-    //砲台を初期状態にするコマンド
-    private void ArmChangeBasic() {
-        while (armSensor.getArmFrontSensor()) {
-            setArmSpeed(0);
+    /*
+    //砲台を初期状態にするコマンド    
+    void ArmChangeBasic(){
+        while(!MaxDown.isFwdLimitSwitchClosed()){               
+            //角度下限認識スイッチが反応したら関数を抜ける
+
+            //角度下限認識スイッチが反応してなかったら、回す
+            ArmMove(Const.ArmBasicSpeed_M);
         }
     }
+    */
 
-    //砲台を地面と平行にするコマンド
-    void ArmChangeParallel() {
+    //---------------------------------------------------------------------
+    //砲台角度制御に関する計算式
 
+    //現在の砲台の角度を計算 
+    //(角度の最大最小差分) ÷（エンコーダー値の最大最小差分) × (エンコーダー現在値 - 最小値) + (角度の最小値)
+    double getArmNow(int ArmNowPoint){
 
+        double NowAngle;
+        NowAngle = Const.ArmAngleError / Const.ArmPointError *
+                (ArmNowPoint - Const.ArmMinPoint) + Const.ArmMinAngle;
+
+        return NowAngle;
     }
 
-    //--------------------------------------------------------------------------------
+    //目標角度に合わせたPIDの目標値を計算
+    //(目標角度 - 最小角度) ×（エンコーダー値の最大最小差分) ÷ (角度の最大最小差分) + (0からの差分)
+    double SetPoint(int TargetAngle){
 
-    //エンコーダー管理
+        double Targetpoint;
+        Targetpoint = (TargetAngle - Const.ArmMinAngle) * Const.ArmMaxPoint /
+                Const.ArmMaxAngle + Const.ArmMinPoint;
 
-
-    //--------------------------------------------------------------------------------
-
-
-    //PID管理
-    //PIDの目標値を設定
-    public void PIDSetPoint(double setPoint) {
+        return  Targetpoint;
     }
 
-    //PIDコントローラーをオン
-    public void PIDEnable() {
+    //目標角度に合わせた重力オフセットを計算
+    //(地面と水平な時の重力オフセット) × (cos目標角度)
+    double SetFeedForward(int TargetAngle){
+
+        double FeedForward;
+        FeedForward = Const.ArmMaxOffset * Math.cos(Math.toRadians(TargetAngle));
+
+        return FeedForward;
     }
 
-    //PIDコントローラーをオフ
-    public void PIDDisable() {
-    }
+    //--------------------------------------------------------------------------------------
 
 
 }
