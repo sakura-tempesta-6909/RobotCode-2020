78b0d6a22198c02c569402a38cc62df73a96b4e7
Hirose-Yuto <heroandmanto0202@gmail.com>
2020-03-01 12:58:21 +0900

    test

--- src/main/java/frc/robot/subClass/Arm.java
+++ src/main/java/frc/robot/subClass/Arm.java
@@ -28,7 +28,7 @@ public class Arm{
     //出力処理
     public void applyState(State state){
 
-        state.nowArmAngle = getArmNow(armSensor.getArmSensorRaw());
+        state.armAngle = getArmNow(armSensor.getArmSensorRaw());
 
         switch(state.armOutState){
             //---------------------------------------------------------------
